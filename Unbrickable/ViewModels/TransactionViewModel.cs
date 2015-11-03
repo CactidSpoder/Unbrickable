60c85c78bb69d06c616f78b67eeab2a4216b2609
JavTheCactus <paolo.merle.javla@gmail.com>
2015-11-04 05:13:22 +0800

    hope this does not rekt anything

---
+++ Unbrickable/ViewModels/TransactionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Unbrickable.ViewModels
+{
+    public class TransactionViewModel
+    {
+        public int account_id { get; set; }
+        public int transaction_status_id { get; set; }
+        public string paypal_transaction_id { get; set; }
+    }
+}
